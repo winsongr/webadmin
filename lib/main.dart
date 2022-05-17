cd995f58b54877a5b3abedd154afb6dd98c57de8
winsongr <winsongr08@gmail.com>
2022-05-17 23:35:38 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "admin",
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,pageTransitionsTheme: PageTransitionsTheme(builders: {ta})
+      ),
+      home: Container(),
+    );
+  }
+}
