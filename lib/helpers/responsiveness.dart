844f0542ccaa26fad545865e30b8975ad12fc86c
winsongr <winsongr08@gmail.com>
2022-05-28 07:36:32 +0530

    layout

--- lib/helpers/responsiveness.dart
+++ lib/helpers/responsiveness.dart
@@ -6,10 +6,47 @@ const int smallScreenSize = 360;
 const int customScreenSize = 1100;
 
 class ResponsiveWidget extends StatelessWidget {
-  const ResponsiveWidget({Key? key}) : super(key: key);
+  final Widget? largeScreen;
+  final Widget? mediumScreen;
+  final Widget? smallScreen;
+  final Widget? customScreen;
+  const ResponsiveWidget(
+      {Key? key,
+       this.largeScreen,
+       this.mediumScreen,
+       this.smallScreen,
+       this.customScreen})
+      : super(key: key);
+
+  static bool isSmallScreen(BuildContext context) =>
+      MediaQuery.of(context).size.width < mediumScreenSize;
+
+  static bool isMediumScreen(BuildContext context) =>
+      MediaQuery.of(context).size.width >= mediumScreenSize &&
+      MediaQuery.of(context).size.width < largeScreenSize;
+
+  static bool isLargeScreen(BuildContext context) =>
+      MediaQuery.of(context).size.width >= largeScreenSize;
+
+  static bool isCustomSize(BuildContext context) =>
+      MediaQuery.of(context).size.width >= mediumScreenSize &&
+      MediaQuery.of(context).size.width <= customScreenSize;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return LayoutBuilder(
+      builder: ((context, constraints) {
+        double width = constraints.maxWidth;
+        if (width >= largeScreenSize) {
+          return largeScreen!;
+        } 
+        else if (width < largeScreenSize && width >= mediumScreenSize) {
+          return mediumScreen!;
+        } 
+        else {
+          return smallScreen!;
+        }
+      }),
+    );
   }
 }
--- lib/layout.dart
+++ lib/layout.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:webadmin/helpers/responsiveness.dart';
 import 'package:webadmin/widgets/large_screen.dart';
 import 'package:webadmin/widgets/small_screen.dart';
+import 'package:webadmin/widgets/top_nav.dart';
 
 class SiteLayout extends StatelessWidget {
-  const SiteLayout({Key? key}) : super(key: key);
-
+  SiteLayout({Key? key}) : super(key: key);
+  final GlobalKey<ScaffoldState> scaffoldkey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-        ),
-        body: const SmallScreen());
+      key: scaffoldkey,
+      appBar: topNavigationBar(context, scaffoldkey),
+      body: const ResponsiveWidget(
+        largeScreen: LargeScreen(),
+        smallScreen: SmallScreen(),
+        mediumScreen: LargeScreen(),
+      ),
+    );
   }
 }
