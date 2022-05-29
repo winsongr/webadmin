56e39a46d546b7e0fe1bb56fda56dccab8cd3398
winsongr <winsongr08@gmail.com>
2022-05-29 20:47:42 +0530

    wer

--- android/app/src/main/kotlin/com/example/webadmin/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.webadmin
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/constants/controllers.dart
+++
@@ -1,5 +0,0 @@
-import 'package:webadmin/controllers/menu_controller.dart';
-import 'package:webadmin/controllers/navigation_controller.dart';
-
-MenuController menuController = MenuController.instance;
-NavigationController navigationController = NavigationController.instance;
